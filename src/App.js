dcb6e81a17ec59616440bbf28f4a638f6a865cb7
yeverycode <ktyjj030677@gmail.com>
2025-08-11 18:17:47 +0900

    [Feat] BasicInfoPage 공통 Header 적용

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Common/Header';
 import BasicInfoPage from './pages/MyPage/BasicInfoPage';
 
 function App() {
   return (
     <Router>
+      {/* 상단 공통 헤더 */}
+      <Header />
+
+      {/* 페이지 라우팅 */}
       <Routes>
         <Route path="/mypage" element={<BasicInfoPage />} />
       </Routes>
